ffc6f287ea318c8532ab0a5a8780ba10c16c8340
Yog-Sotho <monnocellula@gmail.com>
2026-04-25 13:55:30 +0200

    Import ChainConfig and DEXConfig in __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,11 @@
-from .chains import CHAINS, get_chain_config, get_dex_config
+from .chains import CHAINS, get_chain_config, get_dex_config, ChainConfig, DEXConfig
 from .settings import settings
 
 __all__ = [
     "CHAINS",
     "get_chain_config",
     "get_dex_config",
+    "ChainConfig",
+    "DEXConfig",
     "settings",
 ]
