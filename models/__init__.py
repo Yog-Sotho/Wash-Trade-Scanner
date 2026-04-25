9691f55ceb9bbc0e39f5f735a61aba41a4446c44
Yog-Sotho <monnocellula@gmail.com>
2026-04-25 04:02:38 +0200

    Initialize models module with schema imports

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+from .schemas import (
+    Base,
+    SwapTrade,
+    AddressCluster,
+    TokenRiskProfile,
+    DetectionAuditLog,
+    SwapTradeResponse,
+    TokenRiskProfileResponse,
+    AuditRequest,
+    AuditResponse,
+)
+
+__all__ = [
+    "Base",
+    "SwapTrade",
+    "AddressCluster",
+    "TokenRiskProfile",
+    "DetectionAuditLog",
+    "SwapTradeResponse",
+    "TokenRiskProfileResponse",
+    "AuditRequest",
+    "AuditResponse",
+]
